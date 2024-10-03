64a51e0c076b6f2ff30d2c8008e5b9406813eb6d
taked091417 <takeda0708@icloud.com>
2024-10-03 12:43:24 +0900

    Add files via upload

---
+++ HTML/js/Image.js
@@ -0,0 +1,97 @@
+const images = document.querySelectorAll('.bp_image');
+
+let press = false;
+let rect;
+let scale = 1;
+let pos = {
+    x: 0,
+    y: 0,
+};
+let zoompos = {
+    x: 0,
+    y: 0,
+};
+let mousepos = {
+    x: 0,
+    y: 0,
+};
+let mousedrag = {
+    x: 0,
+    y: 0,
+};
+let mousemove = {
+    x: 0,
+    y: 0,
+};
+
+for(var i = 0; i < images.length; i++){
+    images[i].style.transformOrigin = "0 0";
+
+    images[i].addEventListener('mousedown', MouseDown);
+    images[i].addEventListener('wheel', MouseWheel);
+    images[i].addEventListener('mousemove', MouseMove);
+    images[i].addEventListener('mouseout', MouseOut);
+}
+
+function MouseDown(){
+    if (press == false && scale > 1){
+        zoompos.x = pos.x;
+        zoompos.y = pos.y;
+        press = true;
+    }else{
+        press = false;
+    }
+}
+
+function MouseWheel(event){
+    event.preventDefault();// デフォルトの動作をキャンセル
+
+    mousepos = {
+        x:event.clientX,
+        y:event.clientY,
+    };
+
+    if (event.deltaY > 0){
+        scale *= 0.9;
+        pos.x = pos.x * 0.9 + mousepos.x * 0.1;
+        pos.y = pos.y * 0.9 + mousepos.y * 0.1;
+    }else if (event.deltaY < 0){
+        scale *= 1.1;
+        pos.x = pos.x * 1.1 - mousepos.x * 0.1;
+        pos.y = pos.y * 1.1 - mousepos.y * 0.1;
+    }
+
+    if (scale < 1){
+        scale = 1;
+        pos.x = pos.y = 0;
+    }
+    images[event.target.id].style.transform = `translate(${pos.x}px,${pos.y}px) scale(${scale})`
+}
+
+function MouseMove(event){
+    rect = event.target.getBoundingClientRect();
+
+    if (press && scale > 1){
+        mousedrag = {
+            x: event.clientX - rect.left,
+            y: event.clientY - rect.top,
+        };
+
+        pos.x = zoompos.x + (mousemove.x - mousedrag.x) / 1.2;
+        pos.y = zoompos.y + (mousemove.y - mousedrag.y) / 1.2;
+
+        images[event.target.id].style.transform = `translate(${pos.x}px,${pos.y}px) scale(${scale})`
+    }else{
+        mousemove = {
+            x: event.clientX - rect.left,
+            y: event.clientY - rect.top,
+        };
+    }
+}
+
+function MouseOut(event){
+    press = false;
+    scale = 1;
+    pos.x = pos.y = 0;
+    images[event.target.id].style.transform = `translate(${pos.x}px,${pos.y}px) scale(${scale})`
+}
